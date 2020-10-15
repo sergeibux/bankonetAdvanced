0a5040290e791a71cb0f420f1d2a9029c52ff542
Hector <hector.dasse@orange.fr>
2020-10-15 10:20:04 +0200

    INIT Maven

---
+++ src/main/java/com/CESI/bankonetAdvanced/BankonetAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.CESI.bankonetAdvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankonetAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankonetAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CESI/bankonetAdvanced/BankonetAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CESI.bankonetAdvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankonetAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
