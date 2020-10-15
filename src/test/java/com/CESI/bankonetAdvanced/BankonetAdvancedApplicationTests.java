238dd0ccafc595137c5e89422713d7eb1fb8ab6a
sergeibux <sertux.dev@gmail.com>
2020-10-15 11:30:31 +0200

    unit test

--- src/test/java/com/CESI/bankonetAdvanced/BankonetAdvancedApplicationTests.java
+++ src/test/java/com/CESI/bankonetAdvanced/BankonetAdvancedApplicationTests.java
@@ -2,12 +2,31 @@ package com.CESI.bankonetAdvanced;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//more ?
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 class BankonetAdvancedApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
-	void contextLoads() {
+	public void getToto() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/toto").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(content().string(equalTo("toto")));
 	}
 
 }
