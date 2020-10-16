040c4b681c8e1a34e9fe312806795b3224a57339
Hector <hector.dasse@orange.fr>
2020-10-16 15:25:51 +0200

    CREATE COMPTE COURANT

--- src/main/java/com/CESI/accessingdatamysql/Entity/CompteCourant.java
+++ src/main/java/com/CESI/accessingdatamysql/Entity/CompteCourant.java
@@ -1,11 +1,14 @@
 package main.java.com.CESI.accessingdatamysql.Entity;
 
 import main.java.com.CESI.accessingdatamysql.Entity.Compte;
+import main.java.com.CESI.accessingdatamysql.Repository.ClientRepository;
+import main.java.com.CESI.accessingdatamysql.Repository.CompteCourantRepository;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Optional;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class CompteCourant extends Compte {
@@ -31,6 +34,20 @@ public class CompteCourant extends Compte {
         this.IdComteCourant = id;
     }
 
+    public boolean createComtpe(Double decouvertAutorise, String intitule, Double solde, String numero, ClientRepository ClientRepository, CompteCourantRepository CompteCourantRepository, int idClient) {
+        this.montantDecouvertAutorise = decouvertAutorise;
+        this.Intitule = intitule;
+        this.Solde = solde;
+        this.Numero = numero;
 
+        Optional<Client> client = ClientRepository.findById(idClient);
+        if (client.isPresent()) {
+            CompteCourant myCompte = CompteCourantRepository.save(this);
 
+            Client monClient = client.get();
+            monClient.addCompte(myCompte);
+            ClientRepository.save(monClient);
+            return true;
+        } else return false;
+    }
 }
