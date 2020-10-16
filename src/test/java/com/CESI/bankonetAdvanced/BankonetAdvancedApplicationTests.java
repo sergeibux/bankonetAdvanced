13fa0f6b7878688b6bf6e9fd99f4453b7121f5cf
sergeibux <sertux.dev@gmail.com>
2020-10-16 16:22:52 +0200

    i would like to test

--- src/test/java/com/CESI/bankonetAdvanced/BankonetAdvancedApplicationTests.java
+++ src/test/java/com/CESI/bankonetAdvanced/BankonetAdvancedApplicationTests.java
@@ -1,12 +1,13 @@
 package com.CESI.bankonetAdvanced;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class BankonetAdvancedApplicationTests {
 
 	@Autowired
@@ -22,7 +27,7 @@ class BankonetAdvancedApplicationTests {
 	
 	@Test
 	public void getToto() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/toto").accept(MediaType.APPLICATION_JSON))
+		mvc.perform(MockMvcRequestBuilders.get("/demo/toto").accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andExpect(content().string(equalTo("toto")));
 	}
